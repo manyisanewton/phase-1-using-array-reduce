242b4cdaf8331c6760a6a6a77bb1cc8e8483a95e
Newton Manyisa <manyisanewton26@gmail.com>
2024-12-18 08:20:16 +0300

    done

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+// Array of battery batches
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Use reduce() to calculate the total batteries
+const totalBatteries = batteryBatches.reduce((sum, batch) => sum + batch, 0);
+
+// Export the totalBatteries variable for tests
+module.exports = totalBatteries;
+const importedTotalBatteries = require('../index');
+
+describe("Battery Count Tests", () => {
+    it("should have a `totalBatteries` variable", () => {
+        expect(typeof importedTotalBatteries).not.toBe('undefined');
+    });
+
+    it("should have a number as a result", () => {
+        expect(typeof importedTotalBatteries).toBe('number');
+    });
+
+    it("should have made the sum of all the assembled batteries", () => {
+        const expectedTotal = [4, 5, 3, 4, 4, 6, 5].reduce((sum, batch) => sum + batch, 0);
+        expect(importedTotalBatteries).toBe(expectedTotal);
+    });
+});
